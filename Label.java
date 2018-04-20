ba83053847a39f54f9bbcfe612ccfb63fd8cdeb3
Peter Nabil <p.nabil@icloud.com>
2018-04-20 23:41:49 +0200

    Add files via upload

---
+++ Label.java
@@ -0,0 +1,15 @@
+package cse116;
+
+public class Label {
+    Instruction labelname = new Instruction();     // the name given by the user
+    PC position;          // the corresponding position to that label
+    public Label(Instruction labelname,PC position){
+        this.labelname = labelname;
+        this.position = position;
+    }
+    public int findlabel(String s){
+        if (this.labelname.equals(s))
+            return this.position.getPc();
+        return -1000000;
+    }
+}
