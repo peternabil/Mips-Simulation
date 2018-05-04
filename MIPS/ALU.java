bd0b66d50adbc5abe7dc73581f1c15ee382f611e
Seif AbdelHakim <37963932+seiouf97@users.noreply.github.com>
2018-05-04 16:46:39 +0200

    Add files via upload

--- MIPS/ALU.java
+++ MIPS/ALU.java
@@ -1,40 +1,59 @@
-package MIPS;
+package alu.main;
 
 public class ALU {
+
     private register r1;
     private register r2;
     private int ALUSrc;
     private int address;
+    private int shamt;
     private String AluControlSignal;
-    
-    public ALU(register r1 , register r2 , int ALUSrc , int address) {
+    int result;
+
+    public ALU(register r1, register r2, int ALUSrc, int address, int shamt) {
         this.r1 = r1;
         this.r2 = r2;
         this.ALUSrc = ALUSrc;
         this.address = address;
-        int result = 0;
-        
-        if (ALUSrc == 1){
-            result = r1.value + this.address;
-        }
-        
-        else if (ALUSrc == 0){
-            switch (AluControlSignal){
-                case "000":
+        this.shamt = shamt;
+        this.result = 0;
+
+        if (ALUSrc == 1) //i-type instructions
+        {
+            if (AluControlSignal == "0010") //lw , sw , addi
+            {
+                result = r1.value + this.address;
+            } 
+            else if (AluControlSignal == "0111") //slti
+            {
+                if (r1.value < this.address)
+                    result = 1;
+            }
+        } 
+        else if (ALUSrc == 0)//r-type instructions
+        {
+            switch (AluControlSignal) {
+                case "0000": //and
                     result = r1.value & r2.value;
                     break;
-                case "001":
+                case "0001": //or
                     result = r1.value | r2.value;
                     break;
-                case "010":
+                case "0010": //add
                     result = r1.value + r2.value;
-                case "110":
+                    break;
+                case "0110": //sub
                     result = r1.value - r2.value;
-                case "111":
+                    break;
+                case "0111": //slt
                     if (r1.value < r2.value)
                         result = 1;
-                    else
-                        result = 0;
+                    break;
+                case "1100"://nor
+                    result = ~(r1.value | r2.value);
+                    break;
+                case "1111"://suppose "1111" is sll
+                    result = r1.value << shamt;
                     break;
             }
         }
@@ -70,5 +89,5 @@ public class ALU {
 
     public void setAluControlSignal(String AluControlSignal) {
         this.AluControlSignal = AluControlSignal;
-    } 
+    }
 }
